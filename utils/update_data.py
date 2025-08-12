43d6d127f60230a79c1ad19a57c2af9a4eb494b5
Woohyuk Song <harrywsong@naver.com>
2025-08-11 22:37:04 -0400

    final test

--- utils/update_data.py
+++ utils/update_data.py
@@ -29,7 +29,8 @@ def get_driver():
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
 
-        service = Service(ChromeDriverManager().install())
+        service = Service(ChromeDriverManager(arch='arm64').install())
+        # service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
         yield driver
     finally:
